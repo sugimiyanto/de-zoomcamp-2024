a8a8f1900a851d869d935916d5d5f1e70f5c8095
sugimiyanto <sugimiyanto@puc.com>
2024-02-08 05:00:28 +0800

    week 2 homework

---
+++ week2/homework/write_to_gcs_partitioned_parquet.py
@@ -0,0 +1,38 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/mage-zoomcamp-svc-account.json"
+
+bucket_name = 'mage-sugi-learning'
+folder_name = "green_taxi"
+project_id = "sugi-learn"
+root_path = f'{bucket_name}/{folder_name}'
+
+@data_exporter
+def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a Google Cloud Storage bucket.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
+    """
+    table = pa.Table.from_pandas(df)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['lpep_pickup_date'],
+        filesystem=gcs
+    )
+
